9bce1492049b43f51a41a9d01c2da75ad4a8ecf3
Balsz <filecloudrom@gmail.com>
2025-09-15 18:11:29 +0700

    Update and rename arrow_X00T.mk to havoc_X00T.mk

--- havoc_X00T.mk
+++ havoc_X00T.mk
@@ -10,19 +10,22 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common ArrowOS stuff
-$(call inherit-product, vendor/arrow/config/common.mk)
+$(call inherit-product, vendor/havoc/config/common_full_phone.mk)
 
+# Havoc
+WITH_GAPPS= true
+TARGET_INCLUDE_LAWNCHAIR= false
+TARGET_NOT_USES_BLUR= false
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_FACE_UNLOCK_SUPPORTED := true
-DEVICE_MAINTAINER := ∂σткιт#𝙀𝙒
-DEVICE_MAINTAINER_LINK := https://t.me/queenserenade
-TARGET_SUPPORTS_QUICK_TAP := false
+DEVICE_MAINTAINER := BllsszzXD 
+TARGET_SUPPORTS_QUICK_TAP := true
 
 # Inherit from X00T device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := arrow_X00T
+PRODUCT_NAME := havoc_X00T
 PRODUCT_DEVICE := X00T
 PRODUCT_BRAND := asus
 PRODUCT_MODEL := Zenfone Max Pro M1
